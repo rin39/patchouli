e4953116d011cd667f83519f93a32a06c9239e72
rin39 <89615859+rin39@users.noreply.github.com>
2023-01-14 19:45:35 +0300

    AppHead fix

--- components/util/AppHead.tsx
+++ components/util/AppHead.tsx
@@ -5,9 +5,12 @@ type AppHeadProps = {
 };
 
 export default function AppHead({ title }: AppHeadProps) {
+  let pageTitle = "Patchouli";
+  if (title) pageTitle = `Patchouli - ${title}`;
+
   return (
     <Head>
-      {title ? <title>Patchouli - {title}</title> : <title>Patchouli</title>}
+      <title>{pageTitle}</title>
       <meta name="description" content="Library management application" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
