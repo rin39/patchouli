6c4045836e5f42948823168f7158a2b1002ed9d7
rin39 <89615859+rin39@users.noreply.github.com>
2023-02-18 19:29:40 +0300

    Layout component

---
+++ components/ui/Layout.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import AppHead from "../util/AppHead";
+import UserInfo from "./UserInfo";
+
+type LayoutProps = {
+  title?: string;
+} & React.PropsWithChildren;
+
+export default function Layout({ children, title }: LayoutProps) {
+  return (
+    <>
+      <AppHead title={title} />
+      <header className="flex justify-between items-center p-3 ">
+        <Link href="/dashboard" className="text-xl text-fuchsia-900">
+          Patchouli
+        </Link>
+        <UserInfo />
+      </header>
+      <main className="p-3">{children}</main>
+    </>
+  );
+}
