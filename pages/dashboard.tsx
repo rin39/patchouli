432977cf6966eefe11d7cbc4ad5a4db8ba97b006
rin39 <89615859+rin39@users.noreply.github.com>
2023-02-18 19:33:21 +0300

    Display libraries on dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,23 +1,77 @@
-import UserInfo from "../components/ui/UserInfo";
-import AppHead from "../components/util/AppHead";
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession as getServerSession } from "next-auth/next";
+import Link from "next/link";
+import { useState } from "react";
+import Layout from "../components/ui/Layout";
+import { getLibraries } from "../services/libraries";
+import { Library } from "../types/validation";
+import { authOptions } from "./api/auth/[...nextauth]";
 
-export default function Dashboard() {
+type DashboardProps = {
+  libraries: Library[];
+};
+
+export default function Dashboard({ libraries }: DashboardProps) {
+  const [selectedLibrary, setSelectedLibrary] = useState<Library | null>(null);
+
   return (
-    <>
-      <AppHead title="Dashboard" />
-      <header className="flex justify-between items-center p-3 ">
-        <div className="text-xl text-fuchsia-900">Patchouli</div>
-        <UserInfo />
-      </header>
-      <main className="p-3">
-        <aside>
-          <div>Libraries</div>
-          <button className="bg-fuchsia-900 hover:bg-fuchsia-700 text-white p-1 rounded-md w-24">
+    <Layout title="Dashboard">
+      <div className="flex">
+        <aside className="w-32">
+          <ul>
+            {libraries.map((library) => (
+              <li key={library._id}>
+                <button
+                  className="w-full text-left"
+                  onClick={() => setSelectedLibrary(library)}
+                >
+                  {library.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <Link
+            href="/libraries/new"
+            className="bg-fuchsia-900 hover:bg-fuchsia-700 text-white p-0.5 rounded-md w-full block text-center"
+          >
             Add New
-          </button>
+          </Link>
         </aside>
-        <section></section>
-      </main>
-    </>
+        <section className="flex grow">
+          {selectedLibrary && (
+            <table className="w-full">
+              <thead>
+                <tr className="text-left">
+                  {selectedLibrary.fields.map((field, index) => (
+                    <th key={index}>{field.name}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          )}
+        </section>
+      </div>
+    </Layout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (!session || !session.user) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  const libraries = await getLibraries(session?.user?.id);
+
+  return {
+    props: {
+      libraries,
+    },
+  };
+};
