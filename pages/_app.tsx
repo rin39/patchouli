c7e04c705dbdfe7b5cd4e1acf8e89575470263d6
rin39 <89615859+rin39@users.noreply.github.com>
2023-03-04 15:52:04 +0300

    Configured React Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
